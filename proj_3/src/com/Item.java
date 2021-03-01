21fac9999616c34560f13d27b72710a0d3e3bfc4
krushi1403 <kyadu9646@gmail.com>
2021-03-01 19:55:41 +0530

    connected to Database

--- proj_3/src/com/Item.java
+++ proj_3/src/com/Item.java
@@ -27,6 +27,52 @@ public class Item {
 	
 	/*End of Testing Database connection*/
 	
-	/*Insert Item to the Database*/
+	/*Method to Insert Item to the Database*/
+	
+	public String insertItem(String code, String name, String price, String desc)
+	{
+		
+		String output = "";
+		
+		try
+		{
+			
+			Connection con = connect();
+			
+			if (con == null)
+			{
+				return "Error while connecting to the database";
+			}
+	
+			String query = " insert into items(`itemId`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`)" + " values (?, ?, ?, ?, ?)";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+	
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+	
+	
+			preparedStmt.execute();
+			
+			con.close();
+			
+			output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+			
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
+	/*End of Method */
+	
+	
 
 }
